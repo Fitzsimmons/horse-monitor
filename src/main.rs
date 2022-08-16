46e5d480524cf3b7c34c09c47ca3dd2d7fd19acc
fitzsimmons <justin.f@roserocket.com>
2022-08-16 19:55:08 -0400

    Adds separator

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use clipboard::ClipboardContext;
 
 lazy_static! {
     static ref SPEED_REGEX: Regex = Regex::new(r#"\{Base: ([\d\.]+)d, Name: "minecraft:generic\.movement_speed"\}"#).unwrap();
-    // static ref SPEED_REGEX: Regex = Regex::new(r#"{Base: ([\d\.]+)d, Name: "minecraft:generic\.movement_speed"}"#).unwrap();
     static ref JUMP_REGEX: Regex = Regex::new(r#"\{Base: ([\d\.]+)d, Name: "minecraft:horse\.jump_strength"\}"#).unwrap();
     static ref HEALTH_REGEX: Regex = Regex::new(r#"\{Base: ([\d\.]+)d, Name: "minecraft:generic\.max_health"\}"#).unwrap();
 }
@@ -24,21 +23,30 @@ struct Handler {
 
 impl ClipboardHandler for Handler {
     fn on_clipboard_change(&mut self) -> CallbackResult {
+        let mut printed = false;
+
         let s = self.ctx.get_contents().unwrap();
 
         if let Some(speed_capture) = SPEED_REGEX.captures(&s) {
             let speed: f64 = speed_capture.get(1).unwrap().as_str().parse().unwrap();
             println!("Speed:  {0:.2}", speed / SPEED_MAX * 100.0);
+            printed = true;
         }
 
         if let Some(jump_capture) = JUMP_REGEX.captures(&s) {
             let jump: f64 = jump_capture.get(1).unwrap().as_str().parse().unwrap();
             println!("Jump:   {0:.2}", jump / JUMP_MAX * 100.0);
+            printed = true;
         }
 
         if let Some(health_capture) = HEALTH_REGEX.captures(&s) {
             let health: f64 = health_capture.get(1).unwrap().as_str().parse().unwrap();
             println!("Health: {0:.2}", health / HEALTH_MAX * 100.0);
+            printed = true;
+        }
+
+        if printed {
+            println!("------------------");
         }
 
         CallbackResult::Next
